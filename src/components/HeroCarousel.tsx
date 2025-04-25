1f31c933ab88cd1c10b54620bdc4954b47be30e8
gitBakr <bakrbackend@gmail.com>
2025-04-25 10:33:15 +0200

    Fix: Move images to public folder

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -5,16 +5,12 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import image1 from "@/assets/37d51d1a-e507-4611-a68a-3a7345fe2ef8.png";
-import image2 from "@/assets/bc6b38fd-424d-4a3c-b306-8edf0d5bcb0d.png";
-import image3 from "@/assets/fc430996-5c3f-4b8b-b01f-07f3ecdff192.png";
-import image4 from "@/assets/7473111b-451f-42b6-9f3f-ad06220c978f.png";
 
 const heroImages = [
-  image1,
-  image2,
-  image3,
-  image4
+  "/images/37d51d1a-e507-4611-a68a-3a7345fe2ef8.png",
+  "/images/bc6b38fd-424d-4a3c-b306-8edf0d5bcb0d.png",
+  "/images/fc430996-5c3f-4b8b-b01f-07f3ecdff192.png",
+  "/images/7473111b-451f-42b6-9f3f-ad06220c978f.png"
 ];
 
 export const HeroCarousel = () => {
